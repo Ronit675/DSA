e9f1702388d5ab4cbf6fc10f17ba3d8ef9297b2f
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-08-28 00:36:33 +0530

    Update TrappingRainWater.cpp
    # Intuition
    <!-- Describe your first thoughts on how to solve this problem. 
    --> First thought was to iterate to count the trapping rainwater.
    
    # Approach
    <!-- Describe your approach to solving the problem.
    --> Take 4 vaiables = l, r, lmax, rmax.
    --> move l to r.
    --> first compare l and r with each other and, then compare with their maxes.
    --> if max is greater then add difference of them in 5th variable else assign that as new maximum.

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
+
+        //left max and right max keeps track of maximum's of both ends.
         int leftmax = 0;
         int rightmax = 0;
+        
+        //moving pointers
         int i = 0; 
         int j = n-1;
+
+        //count of trapping water
         int cnt = 0;
+
         while(i<j){
+
+            //comparing heights first and then compare maximum according to the pointer.
+            //if left pointer is smaller then again compare with leftmax.
+            //if leftmax is smaller then count else assign new leftmax.
             if(height[i] <= height[j]){
                 if(height[i] < leftmax){
                     cnt += min(leftmax, height[j]) - height[i];
@@ -15,8 +26,10 @@ public:
                 else{
                     leftmax = height[i];
                 }
-                    i++;
+                i++;
             }
+            //if right pointer is smaller then again compare with rightmax.
+            //if rightmax is smaller then count else assign new rightmax.
             else{
                 if(height[j] < rightmax){
                     cnt+= min(rightmax, height[i]) - height[j];
@@ -24,7 +37,7 @@ public:
                 else{
                     rightmax = height[j];
                 }
-                    j--;
+                j--;
             }
         }
         return cnt;
