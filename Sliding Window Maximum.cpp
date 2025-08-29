869126baf74193a7e5926cd4433d0825eb7dd305
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-08-29 17:27:20 +0530

    Document sliding window maximum algorithm

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,3 +1,23 @@
+// # Intuition
+// Describe your first thoughts on how to solve this problem. 
+// first thought is to create a window and move from left to right.
+// And to find the maximum element priority queue data structure fits well
+
+// # Approach
+// Describe your approach to solving the problem
+// Approach was to keep pushing right pointer elmenets in the priority queue. 
+// Push the topmost element on the priority queue.
+// If the topmost element is out of range (falls back then the left pointer then perform pop operation till the topmost element comes in range)
+
+// # Complexity
+// - Time complexity:
+// O(Nlogk)
+
+// - Space complexity:
+// O(n)
+
+// # Code
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
